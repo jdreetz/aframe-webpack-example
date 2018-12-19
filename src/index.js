81afcca26475051539bcf3da38d1d642d110ef77
James Reetzke <jreetzke@visiblemeasures.com>
2018-12-19 08:33:44 -0500

    generate components from class. handle scene click and text change with ecs components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import { registerComponent } from 'aframe';
 
-class App {
-  constructor (config = {}) {
-    console.log('app initialized', config);
+function clickToStateComponent(handler) {
+  return {
+    init() {
+      this.el.addEventListener('click', handler);
+    }
   }
 }
 
-new App({ foo: 'bar' });
+class App {
+  constructor (config = {}) {
+    this.createComponents();
+    this.dirty = false;
+    this.currentText = 'foo';
+  }
+
+  createComponents() {
+    registerComponent('overlay-text', this.getComponentInterface());
+    registerComponent('toggle-states', clickToStateComponent(this.toggleState.bind(this)));
+  }
+
+  toggleState(event) {
+    this.dirty = true;
+    this.currentText = 'bar'
+  }
+
+  getComponentInterface() {
+    const app = this;
+
+    return {
+      tick() {
+        if (app.dirty) {
+          this.el.setAttribute('text', app.textVal);
+          app.dirty = false;
+        }
+      }
+    }
+  }
+
+  get textVal() {
+    return `color: white; align: center; value: ${this.currentText}; width: 10; height: auto`;
+  }
+}
+
+new App();
